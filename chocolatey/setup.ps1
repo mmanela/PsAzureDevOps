6f5deef2e245b57c9283b0e9121cdd5ef1b2b2b3
mmanela <mmanela@gmail.com>
2014-11-10 22:09:40 -0800

    Update deploy

--- chocolatey/setup.ps1
+++ chocolatey/setup.ps1
@@ -1,26 +1,33 @@
 function Install-PsVso($here) {
-    $PsVsoPath=Join-Path $env:AppData PsVso
+
+
+    $ModulePaths = @($env:PSModulePath -split ';')
+
+    $ExpectedUserModulePath = Join-Path -Path ([Environment]::GetFolderPath('MyDocuments')) -ChildPath WindowsPowerShell\Modules
+    $Destination = $ModulePaths | Where-Object { $_ -eq $ExpectedUserModulePath}
+
+    if (-not $Destination) {
+        $Destination = $ModulePaths | Select-Object -Index 0
+    }
+
+    if (-not (Test-Path $Destination)) {
+        New-Item $Destination -ItemType Directory -Force | Out-Null
+    } elseif (Test-Path (Join-Path $Destination "PsVso")) {
+        Remove-Item (Join-Path $Destination "PsVso") -Recurse -Force
+    }
+
+    $PsVsoPath=Join-Path $Destination "PsVso"
     if(!(test-Path $PsVsoPath)){
         mkdir $PsVsoPath
     }
-     
-    foreach($folder in (Get-ChildItem $here | ?{ $_.PSIsContainer })){
-        $target=Join-Path $PsVsoPath $folder.BaseName
-        if(test-Path $target){
-            Remove-Item $target -Recurse -Force
-        }
-    }
-    
-    Copy-Item "$here\*" $PsVsoPath -Recurse -Force -Exclude ChocolateyInstall.ps1, Setup.*
 
-    PersistPsVsoPathToEnvironmentVariable "PSModulePath"
-   
+    Copy-Item "$here/*" $PsVsoPath -Recurse -Force -Exclude ChocolateyInstall.ps1, Setup.*
+
     $successMsg = @"
 The PsVso Module has been copied to $PsVsoPath and added to your Module path. 
 You will need to open a new console for the path to be visible.
-To import the module and see its command do the following:
+To import the module so
 PS:>Import-Module PsVso
-PS:>Get-Command -Module PsVso.*
 
 To find more info visit https://github.com/mmanela/PsVso or use:
 PS:>Import-Module PsVso
@@ -28,28 +35,4 @@ PS:>Get-Help PsVso
 "@
     Write-Host $successMsg
 
-}
-
-
-function PersistPsVsoPathToEnvironmentVariable($variableName){
-    $value = [Environment]::GetEnvironmentVariable($variableName, 'User')
-    if($value){
-        $values=($value -split ';' | ?{ !($_.ToLower() -match "\\PsVso$")}) -join ';'
-        $values+=";$PsVsoPath"
-    } 
-    elseif($variableName -eq "PSModulePath") {
-        $values=[environment]::getfolderpath("mydocuments")
-        $values +="\WindowsPowerShell\Modules;$PsVsoPath"
-    }
-    else {
-        $values ="$PsVsoPath"
-    }
-    if(!$value -or !($values -contains $PsVsoPath)){
-        $values = $values.Replace(';;',';')
-        [Environment]::SetEnvironmentVariable($variableName, $values, 'User')
-        $varValue = Get-Content env:\$variableName
-        $varValue += ";$PsVsoPath"
-        $varValue = $varValue.Replace(';;',';')
-        Set-Content env:\$variableName -value $varValue
-    }
 }
