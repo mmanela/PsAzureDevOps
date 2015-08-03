66e47559727bc834a2536118d1a66b4d4453f155
Matthew Manela <mmanela@gmail.com>
2015-08-03 11:12:24 -0700

    remove deprecated function

--- chocolatey/chocolateyinstall.ps1
+++ chocolatey/chocolateyinstall.ps1
@@ -2,7 +2,6 @@ $tools = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 . (Join-Path $tools Setup.ps1)
 try { 
     Install-PsVso "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-    Write-ChocolateySuccess "PsVso"
 } catch {
     Write-ChocolateyFailure "PsVso" "$($_.Exception.Message)"
     throw 
